3f030efaf436419d0fc859d7274cde99760817c7
Francesco-Matteucci <francescomatteucci31@gmail.com>
2025-11-08 12:57:01 +0100

    feat: added Consiglieri section with JSON data and dynamic cards

--- src/views/Consiglieri.vue
+++ src/views/Consiglieri.vue
@@ -1,6 +1,27 @@
 <template>
     <div class="container py-5">
         <h1 class="h2 mb-4">Consiglieri</h1>
-        <p>Sezione dedicata ai membri del gruppo consiliare.</p>
+
+        <div class="row g-4">
+            <div class="col-md-4" v-for="p in people" :key="p.name">
+                <div class="card h-100 shadow-sm border-0">
+                    <img v-if="p.photo" :src="p.photo" class="card-img-top" :alt="p.name">
+                    <div class="card-body">
+                        <h3 class="h5 mb-1">{{ p.name }}</h3>
+                        <p class="text-muted small mb-2">{{ p.role }}</p>
+                        <p class="mb-0">{{ p.bio }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+    import people from '../data/consiglieri.json'
+    export default {
+        data() {
+            return { people }
+        }
+    }
+</script>
