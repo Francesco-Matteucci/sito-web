aa3fa66b9908738e14edc9577a208fdb5fb60d2e
Francesco-Matteucci <francescomatteucci31@gmail.com>
2025-11-08 18:22:34 +0100

    fix(iframe): corrected allowfullscreen boolean attribute in Contatti.vue

--- src/views/Contatti.vue
+++ src/views/Contatti.vue
@@ -45,7 +45,7 @@
                     <div class="ratio ratio-16x9 shadow-sm rounded overflow-hidden">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2998.963083771038!2d12.735!3d42.733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f1e4a3c9d6c7f%3A0xf7e4c8287b4a06b!2sSpoleto%2C%20PG!5e0!3m2!1sit!2sit!4v0000000000000!5m2!1sit!2sit"
-                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            width="600" height="450" style="border:0;" allowfullscreen loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
